a6084173c6c109a81f0125e22e023b81d013a757
Lucas Cristiano <lucas.cristiano0690@gmail.com>
2026-03-17 02:17:55 -0300

    adiciona layout aos botoes

--- script.js
+++ script.js
@@ -3,26 +3,52 @@ const focoBt = document.querySelector(".app__card-button--foco");
 const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
+const titulo = document.querySelector(".app__title");
+const botoes = document.querySelectorAll(".app__card-button");
 
 focoBt.addEventListener("click", () => {
   alterarContexto("foco");
+  focoBt.classList.add("active");
   /*  html.setAttribute("data-contexto", "foco");
   banner.setAttribute("src", "/imagens/foco.png"); */
 });
 
 curtoBt.addEventListener("click", () => {
   alterarContexto("descanso-curto");
+  curtoBt.classList.add("active");
   /*   html.setAttribute("data-contexto", "descanso-curto");
   banner.setAttribute("src", "/imagens/descanso-curto.png"); */
 });
 
 longoBt.addEventListener("click", () => {
   alterarContexto("descanso-longo");
+  longoBt.classList.add("active");
   /*   html.setAttribute("data-contexto", "descanso-longo");
   banner.setAttribute("src", "/imagens/descanso-longo.png"); */
 });
 
 function alterarContexto(contexto) {
+  botoes.forEach(function (contexto) {
+    contexto.classList.remove("active");
+  });
   html.setAttribute("data-contexto", contexto);
   banner.setAttribute("src", `/imagens/${contexto}.png`);
+  switch (contexto) {
+    case "foco":
+      titulo.innerHTML = `Otimize sua produtividade,
+          <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+
+      break;
+    case "descanso-curto":
+      titulo.innerHTML = `Que tal dar uma respirada?
+          <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+
+      break;
+    case "descanso-longo":
+      titulo.innerHTML = `Hora de voltar à superfície.
+          <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+
+    default:
+      break;
+  }
 }
